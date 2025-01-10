c6add9ea957735f356409944dbec4bfd5f7c8891
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-01-11 08:17:43 +0900

    add doc

--- src/CesiumViewer.jsx
+++ src/CesiumViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Viewer, Ion } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import CesiumViewer from './CesiumViewer.jsx'
-createRoot($('CesiumViewer')).render(<CesiumViewer />)
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+createRoot($('CesiumViewer')).render(
+    <BrowserRouter>
+      <Routes>
+      <Route
+          path='/doc'
+          element={
+            <a href='/Documentation/index.html' target='_blank' rel='noopener noreferrer'>
+              Documentation
+            </a>
+          }
+        />
+        <Route path='/' element={<CesiumViewer />} />
+      </Routes>
+    </BrowserRouter>,
+)
